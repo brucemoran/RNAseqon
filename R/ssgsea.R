3692e671fbce4b560e76bc8b9124b50be8edff8b
brucemoran <bruce01campus@gmail.com>
2022-07-25 16:58:07 +0100

    else

--- R/ssgsea.R
+++ R/ssgsea.R
@@ -45,15 +45,19 @@ ssgsea_pca <- function(pways, log2tpm_mat, msigdb_cat = "H", output_dir, contras
                               metadata = metadata,
                               returnData = FALSE)
 
-    readr::write_tsv(tibble::as_tibble(ssgsea_res, rownames = rns), file = paste0(out_dir, "/data/", contrast , ".ssgsea_res.tsv"))
-    if(!is.null(plot_out)){
-      ggp[[1]]
+    if(!is.null(ggp)){
+      readr::write_tsv(tibble::as_tibble(ssgsea_res, rownames = rns), file = paste0(out_dir, "/data/", contrast , ".ssgsea_res.tsv"))
+      if(!is.null(plot_out)){
+        ggp[[1]]
+      }
+      ggplot2::ggsave(ggp[[1]], file = paste0(out_dir, "/plots/", contrast , ".ssgsea_rotationPCA.pdf"))
+      ssgsea_ggp_list <- list(ssgsea_res = ssgsea_res, ggplot_pca = ggp)
+      return(ssgsea_ggp_list)
+    } else {
+      print("No genesets in pathways, skipping...")
     }
-    ggplot2::ggsave(ggp[[1]], file = paste0(out_dir, "/plots/", contrast , ".ssgsea_rotationPCA.pdf"))
-    ssgsea_ggp_list <- list(ssgsea_res = ssgsea_res, ggplot_pca = ggp)
-    return(ssgsea_ggp_list)
   } else {
-    print("No genesets in pathways, skipping...")
+      print("No genesets in pathways, skipping...")
   }
 }
 
